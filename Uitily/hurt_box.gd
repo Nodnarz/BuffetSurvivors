f788c4f8e54ee2ef223a21c113768a2349a84add
will <willnellaponte@gmail.com>
2024-01-05 16:54:24 -0500

    Ice Spear

--- Uitily/hurt_box.gd
+++ Uitily/hurt_box.gd
@@ -23,6 +23,9 @@ func _on_area_entered(area):
 						area.tempdisbale()
 			var damage = area.damage
 			emit_signal("hurt",damage)
+			if area.has_method("enemy_hit"):
+				area.enemy_hit(1)
+			
 
 
 func _on_disable_timer_timeout():
